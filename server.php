058c394c650e773141ebfb2b4e1bc05d5e180226
Ghaith-ElHamwi <ghaithhamwi123@outlook.com>
2024-06-09 12:06:06 +0100

    Setup the server: Implemented server.bat for running the PHP server and updated server.php

--- server.php
+++ server.php
@@ -1,5 +1,7 @@
 <?php
 
+use Ghaith8bit\WebServer\Request;
+use Ghaith8bit\WebServer\Response;
 use Ghaith8bit\WebServer\Server;
 
 require_once 'bootstrap.php';
@@ -9,11 +11,11 @@ $port = $argv[1] ?? $_ENV['PORT'] ?? 8000;
 try {
     $server = new Server('127.0.0.1', $port);
 
+    echo "Server started on http://127.0.0.1:$port\n";
+
     $server->listen(function (Request $request) {
         return new Response('Response Message');
     });
-
-    echo "Server started on http://127.0.0.1:$port\n";
 } catch (\Exception $e) {
     echo 'Error: ' . $e->getMessage() . "\n";
 }
